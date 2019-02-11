7bd776b21f5149acf046e7ad4386bf0ea32dd43d
msuser999 <matias.seppanen@aalto.fi>
2019-02-11 17:34:43 +0200

    json to dataset

--- java_KafkaTesting/src/sparkTesting/SparkMain.java
+++ java_KafkaTesting/src/sparkTesting/SparkMain.java
@@ -1,69 +1,31 @@
 package sparkTesting;
 
-// structured streaming 
-import org.apache.spark.sql.*;
-import org.apache.spark.sql.streaming.StreamingQuery;
-import org.apache.spark.sql.streaming.StreamingQueryException;
-import org.apache.spark.api.java.function.FlatMapFunction;
-
-// streaming 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.*; // JavaStreamingContext, JavaDStream...
-import org.apache.spark.streaming.kafka010.*; // KafkaUtils
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.*;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.clients.producer.*;
-
 import java.util.*;
 
 public class SparkMain {
 
 	public static void main(String[] args) throws StreamingQueryException {
-		System.setProperty("hadoop.home.dir", "C:/Hadoop/"); // location of /bin/winutils.exe (Hadoop on windows...)
 		System.out.println("start");
-		sparkTest2();
+		sparkStreamingTest();
 		System.out.println("end");
-	}
-
-	// SPARK STRUCTURED STREAMING
-	public static void sparkTest1() {
-		// https://spark.apache.org/docs/latest/api/java/org/apache/spark/sql/SparkSession.html
-		SparkSession spark = SparkSession.builder().appName("Ex1").master("local").getOrCreate();
-
-		// Read from socket (create Dataframe)
-		/*	.readStream() => https://spark.apache.org/docs/latest/api/java/org/apache/spark/sql/streaming/DataStreamReader.html
-		 * 	.format(String src) - Specify the data format
-		 * 	.option(String key, String/long/double/boolean value) - Adds an input option for the underlying data source
-		 * 	.load() - Loads input data stream in as a DataFrame, for data streams that don't require a path 	
-		 *  	=> https://spark.apache.org/docs/2.4.0/api/java/index.html?org/apache/spark/sql/Dataset.html		*/
-		Dataset<Row> lines = spark.readStream().format("socket").option("host", "localhost").option("port", 9999).load();
-
-		// Split the lines into words
-		/* .as(Encoder<U> encoder) -  Returns a new Dataset where each record has been mapped on to the specified type
-		 * .flatmap(FlatMapFunction<T, U> f, Encoder<U> encoder) -  (Java-specific) Returns a new Dataset by first applying a function to all elements of this Dataset, and then flattening the results		*/
-		Dataset<String> words = lines.as(Encoders.STRING()).flatMap((FlatMapFunction<String, String>) x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
-		Dataset<Row> wordCounts = words.groupBy("value").count(); // running word count
-
-		// Start running the query that prints the running counts to the console
-		/*	.writeStream() => http://spark.apache.org/docs/2.4.0/api/java/index.html?org/apache/spark/sql/streaming/DataStreamWriter.html
-		 * 	.outputMode(String outputMode) - Specifies how data of a streaming DataFrame/Dataset is written to a streaming sink
-		 * 	.format(String src) - Specifies the underlying output data source
-		 * 	.start() - Starts the execution of the streaming query, which will continually output results to the given path as new data arrives		*/
-		StreamingQuery query = wordCounts.writeStream().outputMode("complete").format("console").start();
-
-		try {
-			query.awaitTermination();
-		} catch (StreamingQueryException e) {
-			e.printStackTrace();
-		}
 	}
 
 	// SPARK STREAMING 
 	/*	https://spark.apache.org/docs/latest/streaming-programming-guide.html
 	* 	https://spark.apache.org/docs/2.4.0/streaming-kafka-0-10-integration.html		*/
-	public static void sparkTest2() {
+	public static void sparkStreamingTest() {
 		// Configuration for Kafka consumer
+		String srcTopic = "mytopic";
 		Map<String, Object> kafkaParams = new HashMap<>();
 		kafkaParams.put("bootstrap.servers", "localhost:9092");
 		kafkaParams.put("key.deserializer", StringDeserializer.class);
@@ -71,20 +33,21 @@ public class SparkMain {
 		kafkaParams.put("group.id", "stream1");
 		kafkaParams.put("auto.offset.reset", "latest");
 		kafkaParams.put("enable.auto.commit", false);
-		Collection<String> topics = Arrays.asList("mytopic");
+		Collection<String> topics = Arrays.asList(srcTopic);
 
 		/*	https://spark.apache.org/docs/latest/api/java/org/apache/spark/SparkConf.html
-		 * 	.setAppName(String name) - Set a name for your application.
 		 * 	.setMaster(String master) - The master URL to connect to, such as "local" to run locally with one thread, 
 		 * 		"local[4]" to run locally with 4 cores, or "spark://master:7077" to run on a Spark standalone cluster. 		*/
-		SparkConf conf = new SparkConf().setAppName("sparkEx2").setMaster("local"); // "local" or "local[*]" ?
+		SparkConf conf = new SparkConf().setAppName("sparkEx2").setMaster("local[2]"); // "local" or "local[*]" or local[2] ?
 
 		/*	https://spark.apache.org/docs/latest/api/java/index.html?org/apache/spark/streaming/api/java/JavaStreamingContext.html
 		 	A Java-friendly version of StreamingContext which is the main entry point for Spark Streaming functionality. It provides methods to create JavaDStream and JavaPairDStream from input sources	*/
 		JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(4000));
-		JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(ssc, LocationStrategies.PreferConsistent(), ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
-		/* 	Print received message
+		ssc.sparkContext().setLogLevel("ERROR"); // Redirect INFO messages to logs instead of spamming the console
+
+		/* 	Create DStream
 		 	https://spark.apache.org/docs/latest/api/java/index.html?org/apache/spark/streaming/api/java/JavaDStream.html 		*/
+		JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(ssc, LocationStrategies.PreferConsistent(), ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
 		JavaDStream<String> lines = stream.map(r -> r.value().toString()); // ?? difference between DStream and InputDStream ??
 		lines.print();
 
@@ -96,21 +59,52 @@ public class SparkMain {
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-		// Writing to another Kafka topic
-		// Needed? __If__ sql queries are done on Datasets (?), is writing to Kafka even done from a DStream..?
-		/* 	https://spark.apache.org/docs/latest/rdd-programming-guide.html		*/
-		lines.foreachRDD(rdd -> rdd.foreach(s -> {
-			KafkaProducer<String, String> prod = new KafkaProducer<>(props);
-			prod.send(new ProducerRecord<String, String>("mytopic2", s + " @ " + java.time.Instant.now()));
-			prod.close();
-		}));
-		ssc.start();
+		// Make an SQL query on incoming messages and send results to Kafka
+		/*	https://spark.apache.org/docs/latest/api/java/index.html?org/apache/spark/api/java/JavaRDD.html		
+		 * 	https://spark.apache.org/docs/latest/api/java/org/apache/spark/sql/SparkSession.html
+		 * 	https://spark.apache.org/docs/latest/api/java/org/apache/spark/sql/Dataset.html		*/
+		/*
+		String outputTopic = "mytopic2";
+		KafkaProducer<String, String> prod = new KafkaProducer<>(props);
+		JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(x.split(" ")).iterator()); // Split each line into words
+		words.foreachRDD(rdd -> {
+			// Get the singleton instance of SparkSession
+			SparkSession spark = SparkSession.builder().config(rdd.context().getConf()).getOrCreate(); // sparkContext() (in tutorial) == context() ???
+		
+			// Create a Dataset
+			Dataset<String> ds = spark.createDataset(JavaRDD.toRDD(rdd), Encoders.STRING());
+			ds.createOrReplaceTempView("words"); // table name ?
+		
+			// Make a query on  Dataset
+			Dataset<Row> ds2 = spark.sql("SELECT * FROM words");
+			ds2.show(); // prints the dataset
+		
+			// Send dataset to Kafka
+			List<String> ds3 = ds2.as(Encoders.STRING()).collectAsList();
+			ds3.forEach(e -> prod.send(new ProducerRecord<String, String>(outputTopic, e + " @ " + java.time.Instant.now())));
+		});*/
 
+		// JSON stream testing
+		lines.foreachRDD(rdd -> {
+			SparkSession spark = SparkSession.builder().config(rdd.context().getConf()).getOrCreate();
+			Dataset<String> ds = spark.createDataset(JavaRDD.toRDD(rdd), Encoders.STRING());
+			Dataset<Row> ds2 = spark.read().json(ds);
+			ds2.show();
+		});
+
+		ssc.start();
 		try {
 			ssc.awaitTermination();
+			//prod.close();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 }
 
+/* OLD -- writing from DStream
+lines.foreachRDD(rdd -> rdd.foreach(s -> {
+	KafkaProducer<String, String> prod = new KafkaProducer<>(props);
+	prod.send(new ProducerRecord<String, String>("mytopic2", s + " @ " + java.time.Instant.now()));
+	prod.close();
+}));*/
